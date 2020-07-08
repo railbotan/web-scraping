cfd0a5a677f6691c11f4adb18e20db4d7e0400eb
railbotan@mail.ru <railbotan@mail.ru>
2020-07-08 16:40:06 +0500

    Refactor code

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
-const fs = require("fs");
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 const moreUniversitySelector= "body > div:nth-child(2) > div.maincontent > div.m270 > div > div.mobpadd20.morediv";
 const moreSpecialtiesSelector = "#rezspec > div.mobpadd20.morediv";
 
 const specialtiesSelector = "#rezspec > div.headnap > div.napravlenienaz > center > span.font3 > b";
 const specialtyCodesSelector = "#rezspec > div.headnap > div.napravlenienaz > center > span.font2";
-const notesSelector = "div.mobpadd20-3:nth-child(2) table.cirfloat.cirfloatmargin:first-child table.circ2.circ2unique b.font11";
+const passingScoresSelector = "div.mobpadd20-3:nth-child(2) table.cirfloat.cirfloatmargin:first-child table.circ2.circ2unique b.font11";
+
+async function getMoreElements(page, selector) {
+    while (true) {
+        try {
+            await page.waitForSelector(selector);
+            await page.click(selector);
+        } catch (e) {
+            break;
+        }
+    }
+}
+
+async function getPages(page, links) {
+    const pages = [];
+    let pagesCount = 0;
+    for (const link of links) {
+        await page.goto(link);
+        await getMoreElements(page, moreSpecialtiesSelector);
+        const pageContent = await page.content();
+        console.log(`Получена страница №${++pagesCount}: ${link}`);
+        pages.push(pageContent);
+    }
+    return pages;
+}
 
 async function getLinks(content) {
     const dom = cheerio.load(content);
@@ -20,39 +44,22 @@ async function getLinks(content) {
     return links;
 }
 
-async function pageClicker(page, selector) {
-    while (true) {
-        try {
-            await page.waitForSelector(selector);
-            await page.click(selector);
-        } catch (e) {
-            break;
-        }
-    }
-}
-
 async function getContents(url) {
-    const pages = [];
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    await pageClicker(page, moreUniversitySelector);
+    await getMoreElements(page, moreUniversitySelector);
     const content = await page.content();
     console.log("Получена главная страница");
     const links = await getLinks(content);
-    for (const link of links) {
-        await page.goto(link);
-        await pageClicker(page, moreSpecialtiesSelector);
-        const pageContent = await page.content();
-        console.log(`Получена страница ${link}`);
-        pages.push(pageContent);
-    }
+    const pages = await getPages(page, links);
     await browser.close();
     return pages;
 }
 
 function parsePage(page) {
     const dom = cheerio.load(page);
+
     const name = dom("h2[itemprop=name]").text();
     const alterName = dom("h2[itemprop=alternateName]").text();
 
@@ -64,21 +71,25 @@ function parsePage(page) {
             specialty: data
         };
     });
+
     dom(specialtyCodesSelector).each((i, elem) => {
         const data = dom(elem).text();
         specialties[i] = {
             ...specialties[i],
-            code: data.split(' | ')[1]
+            specialtyCode: data.split(' | ')[1]
         };
     });
-    dom(notesSelector).each((i, elem) => {
+
+    dom(passingScoresSelector).each((i, elem) => {
         const data = dom(elem).text();
         specialties[i] = {
             ...specialties[i],
-            note: data
+            passingScores: data
         };
     });
+
     console.log(`Извлеченны данные для ${alterName}`);
+
     return {
         name,
         alterName,
@@ -86,7 +97,8 @@ function parsePage(page) {
     };
 }
 
-async function getData() {
+async function getDataFromSite() {
+    console.log("Start scraping!");
     const result = [];
     const pages = await getContents("https://tabiturient.ru");
     for (const page of pages) {
@@ -95,15 +107,16 @@ async function getData() {
     return result;
 }
 
-function writeFile(data) {
+function writeDataToFile(data) {
     const json = JSON.stringify(data, null, 2);
-    fs.writeFile("D:\\Практика 3\\web-scraping\\temp.json", json, () => console.log("write!"));
+    fs.writeFile("D:\\Практика 3\\web-scraping\\scores.json", json,
+        () => console.log("Data are writing in file! Finish scraping!"));
 }
 
-getData().then(writeFile);
+getDataFromSite().then(writeDataToFile);
 
-function readFile(data) {
-    const vuz = JSON.parse(data, (key, value) => key === "note" ? parseInt(value) : value);
+function readDataFromFile(data) {
+    const vuz = JSON.parse(data, (key, value) => key === "passingScores" ? parseInt(value) : value);
 }
 
-//fs.readFile("D:\\Практика 3\\web-scraping\\temp.json", "utf-8", (err, data) => readFile(data));
+//fs.readFile("D:\\Практика 3\\web-scraping\\scores.json", "utf-8", (err, data) => readDataFromFile(data));
